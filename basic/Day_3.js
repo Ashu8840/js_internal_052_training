a9458b20f587ecf94555bcb8f5b982d0c7284423
Ayush Tripathi <ayush.bhrg@gmail.com>
2025-12-11 01:31:54 +0530

    Day_3 commit

---
+++ basic/Day_3.js
@@ -0,0 +1,77 @@
+// function get(a) {
+//   return a+2;
+// }
+// console.log(get(5))
+
+// let num = [1, 2, 3, 4, 5, 6,get(5), "Ayush"]
+// console.log(num)
+
+// let fruits = ["Apple", "Banana", "kiwi", "Papaya"]
+// fruits.forEach(element => {
+//   console.log(element)
+// });
+
+// fruits.push("papaya")
+// fruits.pop()
+// fruits.unshift("kiwi")
+// fruits.shift()
+// fruits.includes("Mango")
+// fruits.indexOf("Banana")
+
+// for (let i = 0; i < 5; i++){
+//   console.log(i)
+// }
+
+// let num = [23, 45, 65, 43, 67, 76, 67]
+// let res = num.filter(n => n==67)
+// console.log(res)
+
+// let n = num.map(n => n * 2)
+// console.log(n)
+
+// let gre = num.filter(n => n < 30)
+// console.log(gre)
+
+// let re = num.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+// console.log(re)
+
+//This is all about the map,reduce and filter.
+
+// let num = [2, 4, 6, 8]
+
+// let dou = num.map(n => n * 2)
+// console.log(dou)
+
+// let num1 = [10, 25, 30, 5, 60]
+// let fil = num1.filter(n => n > 20)
+// console.log(fil)
+
+// let num2 = [1, 2, 3, 4, 5]
+
+// let sum = num2.reduce((a, v) => a + v, 0)
+// console.log(sum)
+
+
+// let student = {
+//   name: "Ayush",
+//   age: "22",
+//   city:"Prayagraj"
+// }
+// console.log(student)
+
+// console.log(student.name);
+
+// student.state = "Uttar Pradesh"
+
+// console.log(student)
+
+// delete student.city;
+// console.log(student)
+
+// let result = JSON.stringify(student)
+// console.log(result)
+
+// let obj = JSON.parse(result)
+// console.log(obj)
+
